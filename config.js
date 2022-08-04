18a51c1bb14b18a85715890c1a93282598945ddd
Yxbxrock <102041995+Yxbxrock@users.noreply.github.com>
2022-08-04 18:49:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ let chalk = require('chalk')
 global.owner = [
   ['6287898307350'],
   ['66287898307350'],
-  ['6287898307350', 'gempy', true]  
+  ['6282365089294', 'Owner Karyl Bot', true]  
   
 ] 
-global.ownername = [['Gempy']]
-global.namabot = [['PENTABOT']]
-global.ownerinsta = [['@gmpyyy']]
+global.ownername = [['Owner Karyl Bot']]
+global.namabot = [['Karyl Bot']]
+global.ownerinsta = [['@yxbxrock']]
 global.mods = [] 
 global.prems = [] 
 
@@ -43,8 +43,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = 'PENTABOT'
-  var sticker_author = 'By GEMPY'
+  var sticker_name = 'Created By'
+  var sticker_author = 'Karyl Bot'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -61,7 +61,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = 'Created by Gempy'
+global.wm = 'Created by Karyl Bot'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
